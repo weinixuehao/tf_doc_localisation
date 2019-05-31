29bc472ab8b348b7d1b0b29ff62ab8778d843d94
chenlong <you@example.com>
2019-05-31 16:15:08 +0800

    add opencv sdk

--- android/app/src/main/java/pers/demo/MainActivity.java
+++ android/app/src/main/java/pers/demo/MainActivity.java
@@ -1,7 +1,16 @@
 package pers.demo;
 
+import android.content.Context;
+import android.content.res.AssetManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.media.ThumbnailUtils;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import java.io.IOException;
+import java.io.InputStream;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +18,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Bitmap bitmap = getBitmapFromAsset(this, "receipt_655.jpg");
+        Bitmap bmp = ThumbnailUtils.extractThumbnail(bitmap, 600, 800);
+        DocScanner.getInstance(this).inference(bmp);
+        Log.i("asssss", "asdf");
+    }
+
+    public static Bitmap getBitmapFromAsset(Context context, String filePath) {
+        AssetManager assetManager = context.getAssets();
+
+        InputStream istr;
+        Bitmap bitmap = null;
+        try {
+            istr = assetManager.open(filePath);
+            bitmap = BitmapFactory.decodeStream(istr);
+        } catch (IOException e) {
+            // handle exception
+        }
+
+        return bitmap;
     }
 }
